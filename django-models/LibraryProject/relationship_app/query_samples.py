63d04f0ab8a2de19466f6d7b5a3fa93a5442fc51
abenable <ableabenaitwe@gmail.com>
2025-02-19 12:47:31 +0300

    Refactor book querying functions: retrieve author object before filtering books by author

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,8 +1,9 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 def query_books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
     # Query all books by a specific author using filter
-    return Book.objects.filter(author__name=author_name)
+    return Book.objects.filter(author=author)
 
 def list_library_books(library_name):
     # List all books in a library using filter
@@ -15,18 +16,3 @@ def get_library_librarian(library_name):
         return library.librarian
     except Library.DoesNotExist:
         return None
-
-# Example usage:
-"""
-# Create sample data
-author = Author.objects.create(name="J.K. Rowling")
-book = Book.objects.create(title="Harry Potter", author=author)
-library = Library.objects.create(name="City Library")
-library.books.add(book)
-librarian = Librarian.objects.create(name="John Doe", library=library)
-
-# Run queries
-books = query_books_by_author("J.K. Rowling")
-library_books = list_library_books("City Library")
-librarian = get_library_librarian("City Library")
-"""
