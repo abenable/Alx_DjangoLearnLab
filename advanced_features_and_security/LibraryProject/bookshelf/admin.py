ab148ff306da9840d6ce8824854f92d835859cbd
abenable <ableabenaitwe@gmail.com>
2025-02-28 13:10:13 +0300

    Register CustomUser model with custom admin configuration

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, Book, Library, Author, UserProfile, Librarian
 
-@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     list_display = ('username', 'email', 'is_staff', 'is_active')
     list_filter = ('is_staff', 'is_active', 'groups')
     fieldsets = UserAdmin.fieldsets + (
         ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
     )
+
+admin.site.register(CustomUser, CustomUserAdmin)
 
 @admin.register(Library)
 class LibraryAdmin(admin.ModelAdmin):
