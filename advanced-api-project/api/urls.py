08baf1be2dd04551e7d6eeec2080802f7e3b239f
abenable <ableabenaitwe@gmail.com>
2025-03-19 12:11:31 +0300

    Refactor URL patterns to include CRUD operations for books; remove author URLs

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from .views import BookListCreateView, BookDetailView, AuthorListCreateView, AuthorDetailView
+from .views import (
+    BookListCreateView, BookDetailView, 
+    AuthorListCreateView, AuthorDetailView,
+    CreateView, UpdateView, DeleteView
+)
 
 app_name = 'api'
 
@@ -7,8 +11,7 @@ urlpatterns = [
     # Book URLs
     path('books/', BookListCreateView.as_view(), name='book-list-create'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-    
-    # Author URLs
-    path('authors/', AuthorListCreateView.as_view(), name='author-list-create'),
-    path('authors/<int:pk>/', AuthorDetailView.as_view(), name='author-detail'),
-]
+    path('books/create/', CreateView.as_view(), name='book-create'),
+    path('books/update/', UpdateView.as_view(), name='book-update'),
+    path('books/delete/', DeleteView.as_view(), name='book-delete'),
+    ]
