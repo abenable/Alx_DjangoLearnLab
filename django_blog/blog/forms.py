03adf4a4c2f23748a7ab449dfbc204d04e0af880
abenable <ableabenaitwe@gmail.com>
2025-03-19 13:26:37 +0300

    Remove placeholder attribute from TagWidget in PostForm

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -36,7 +36,7 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ['title', 'content', 'tags']
         widgets = {
-            'tags': TagWidget(attrs={'placeholder': 'Enter tags separated by commas'})
+            'tags': TagWidget()
         }
 
     def clean_tags(self):
