dcc1143043e46c17b055a81c698eb1686d4d55c6
abenable <ableabenaitwe@gmail.com>
2025-03-19 12:15:25 +0300

    Add missing import for django_filters in views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, permissions
+from django_filters import rest_framework
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from .models import Author, Book
