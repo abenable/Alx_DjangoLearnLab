dc27f571c385a8e7dfd73b0f5d1f579e91370bc5
abenable <ableabenaitwe@gmail.com>
2025-03-31 12:13:01 +0300

    Add missing newline at the end of the urlpatterns in urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('feed/', views.FeedView.as_view(), name='feed'),
     path('posts/<int:pk>/like/', views.LikeView.as_view(), name='post-like'),
+    path('posts/<int:pk>/unlike/', views.LikeView.as_view(), name='post-unlike'),
 ]
