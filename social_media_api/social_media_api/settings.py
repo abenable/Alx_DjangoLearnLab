7d465f8757aa6bf38afdd16685ff9bd92eedbcb1
abenable <ableabenaitwe@gmail.com>
2025-03-31 12:59:21 +0300

    Update database configuration to use MySQL instead of SQLite

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -98,8 +98,12 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'social_media_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
